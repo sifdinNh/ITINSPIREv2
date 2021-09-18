0576f90d4c5aea59a0ca5a554bf283f60f74f864
sifdinNh <36017867+sifdinNh@users.noreply.github.com>
2021-09-18 13:02:09 +0100

    first commit

--- backend/itinspire/settings.py
+++ backend/itinspire/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4nd)9xri(t6mjzvdt3*vku(sz7b8ww%ozd)gy0vsfaf=shgej8'
+
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
